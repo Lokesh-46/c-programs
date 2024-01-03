307d8301a0f348a1a609d72940ae4ccb77aae733
Lokesh-46 <153740103+Lokesh-46@users.noreply.github.com>
2024-01-03 12:54:14 +0000

    done

---
+++ vote.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main()
+{
+ int age=15;
+ 
+ if (age < 18) {
+    cout<<" NOT elegible for vote";
+ }else {
+    cout <<"elegible"<<endl;
+ }
+
+    return 0;
+}
